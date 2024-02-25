8a715b4cb0b3ec20ba1ebc0f5b9ced075eb02281
danielgambera <62084650+danielgambera@users.noreply.github.com>
2024-02-25 18:56:11 -0300

    Aula 04

--- src/app/pages/usuarios/listagem-usuarios/listagem-usuarios.component.ts
+++ src/app/pages/usuarios/listagem-usuarios/listagem-usuarios.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from "../../../components/page-title/page-title.component";
+import { UsuariosService } from '../../../services/usuarios.service';
+import { IUsuario } from '../../../interfaces/usuario';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-listagem-usuarios',
     standalone: true,
     templateUrl: './listagem-usuarios.component.html',
     styleUrl: './listagem-usuarios.component.css',
-    imports: [PageTitleComponent]
+    imports: [PageTitleComponent, RouterLink]
 })
 export class ListagemUsuariosComponent {
 
   titulo:string = 'Usuários';
+  usuarios: IUsuario[] = [];
+
+  constructor(private usuariosService: UsuariosService) {}
+
+  ngOnInit()
+  {
+    this.usuariosService.buscarTodosUsuarios().subscribe(usuarios => {
+      this.usuarios = usuarios;
+    }, error => { console.error(error); })
+  }
 
 }
